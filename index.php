c4b7a6550bc6a2a4d15b655122321e2041884438
AchmadBudy <56147721+AchmadBudy@users.noreply.github.com>
2022-04-18 16:05:45 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+require_once 'function.php';
+
+$koneksi = new databasenya;
+
+$datas = $koneksi->tambilData();
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Tugas MySQl</title>
+</head>
+
+<body>
+    <table border="1px">
+        <tr>
+            <th>No</th>
+            <th>Nama Barang</th>
+            <th>Stock</th>
+            <th>Harga Beli</th>
+            <th>Harga Jual</th>
+            <th>Action</th>
+        </tr>
+        <?php $i = 1; ?>
+        <?php foreach ($datas as $data) : ?>
+            <tr>
+                <td><?= $i; ?></td>
+                <td><?= $data['nama_barang']; ?></td>
+                <td><?= $data['stok']; ?></td>
+                <td><?= number_format($data['harga_beli']); ?></td>
+                <td><?= number_format($data['harga_jual']); ?></td>
+                <td>
+                    <a href="ubah.php?id=<?= $data['id_barang']; ?>">Edit</a>
+                    <a href="hapus.php?id=<?= $data['id_barang']; ?>" onclick="alert('Apakah Yakin Untuk Dihapus?')">Hapus</a>
+                </td>
+            </tr>
+            <?php $i++ ?>
+        <?php endforeach; ?>
+    </table>
+
+</body>
+
+</html>
