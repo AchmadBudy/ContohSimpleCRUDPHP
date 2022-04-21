f57e471f5cc1d7d8a70f2644cf6d42796110d3e6
AchmadBudy <56147721+AchmadBudy@users.noreply.github.com>
2022-04-21 15:35:33 +0700

    Update tambah.php
    mempercantik

--- tambah.php
+++ tambah.php
@@ -15,24 +15,65 @@ if (isset($_POST['submit'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tambah Data</title>
+
+    <style>
+        input {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            box-sizing: border-box;
+        }
+
+        .card {
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+            border-radius: 5px;
+            margin: 10px 400px;
+            padding: 8px 50px;
+            background-color: #E7E9EB;
+        }
+
+        .button {
+            background-color: #04AA6D;
+            color: white;
+            padding: 14px 20px;
+            margin: 20px 0;
+            border: none;
+            cursor: pointer;
+            width: 100%;
+        }
+
+        .button-sec {
+            background-color: #83BD75;
+            color: white;
+            padding: 14px 20px;
+            margin: 20px auto;
+            border: none;
+            cursor: pointer;
+            width: 90%;
+            display: block;
+            text-align: center;
+            text-decoration: none;
+        }
+    </style>
 </head>
 
 <body>
-    <form action="" method="POST">
-        <label for="namabarang">Nama Barang : </label>
-        <input type="text" name="namabarang" id="namabarang">
-        <br>
-        <label for="stok">Stok Barang : </label>
-        <input type="number" name="stok" id="stok">
-        <br>
-        <label for="hargabeli">Harga Beli : </label>
-        <input type="number" name="hargabeli" id="hargabeli">
-        <br>
-        <label for="hargajual">Harga Jual : </label>
-        <input type="number" name="hargajual" id="hargajual">
-        <br>
-        <button type="submit" name="submit">Tambah</button>
-    </form>
+    <div class="card">
+        <h2 style="text-align: center;">Tambah Data</h2>
+        <form action="" method="POST">
+            <label for="namabarang">Nama Barang : </label>
+            <input type="text" name="namabarang" id="namabarang">
+            <label for="stok">Stok Barang : </label>
+            <input type="number" name="stok" id="stok">
+            <label for="hargabeli">Harga Beli : </label>
+            <input type="number" name="hargabeli" id="hargabeli">
+            <label for="hargajual">Harga Jual : </label>
+            <input type="number" name="hargajual" id="hargajual">
+            <button type="submit" name="submit" class="button">Tambah</button>
+            <a href="/index.php" class="button-sec">Kembali</a>
+        </form>
+
+    </div>
 </body>
 
 </html>
